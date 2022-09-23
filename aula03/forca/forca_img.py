150d7c3c707f4eed0d40be9a827127de5b5ff6f5
Tayssa Antoniasse <98670493+TayAntony@users.noreply.github.com>
2022-09-23 09:00:12 -0300

    jogo da forca, fazer printar a forca a cada tentativa, interligar dicas com as palavras, substituir os _ pela letra na posição correta

---
+++ aula03/forca/forca_img.py
@@ -0,0 +1,14 @@
+import forca
+
+def forca1():
+    print('''     _______
+    |       |
+    |
+    |
+    |
+    |''')
+    print('>>> Palavra: ', end='' )
+    
+
+if __name__ == '__main__':
+    forca1()
---
+++ aula03/forca/main.py
@@ -0,0 +1,7 @@
+from forca import *
+
+def run():
+    menu()
+
+if __name__ == '__main__':
+    menu()
