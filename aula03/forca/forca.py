8251309451239e1cdb05011e4e6ce6232c1b29a1
Tayssa Antoniasse <98670493+TayAntony@users.noreply.github.com>
2022-10-04 16:53:50 -0300

    verificar letra, encontrar a posição e substituir por underline

--- aula03/forca/forca.py
+++ aula03/forca/forca.py
@@ -1,12 +1,11 @@
-
 import inquirer
 import random
 import forca_img
 import sys
 
-palavra_secreta = []
+palavra_secreta = ''
 anderlines_palavra_secreta = []
-
+palavra_sorteada = False
 def menu():
     while True:
             perguntas = [
@@ -24,7 +23,6 @@ def menu():
 
             if respostas['escolha'] == 'Jogar':
                 jogo_forca()
-            
 
             elif respostas['escolha'] == 'Adicionar/Remover palavra':
                 add_remove()
@@ -32,10 +30,14 @@ def menu():
             elif respostas['escolha'] == 'Visualizar palavras e dicas':
                 view_tips_words()
 
-
             elif respostas['escolha'] == 'Sair':
                 sair()
 
+
+#TODO Verificar se a letra informada existe na palavra
+#TODO Encontrar a posição da letra na palavra
+#TODO Substituir o underline pela letra na posição
+#BINARYSEARCH 
 
 def sortear_palavra():
     palavras = open(r'./forca/palavras.txt', 'r', encoding='utf-8')
@@ -48,14 +50,22 @@ def sortear_palavra():
 
     indice_secret_word = random.randint(0, len(lista_palavras))
     palavra_secreta = lista_palavras[indice_secret_word]
+
     anderlines_palavra_secreta = '_ '*len(palavra_secreta)
     palavras.close()
     print(forca_img.forca1()[0], end='')
     print(anderlines_palavra_secreta)
+    palavra_sorteada = True
     return indice_secret_word, palavra_secreta, anderlines_palavra_secreta
     
 
+def print_forca(anderlines_palavra_secreta, posicao_forca):
+    print(forca_img.forca1()[0], end='')
+    print(anderlines_palavra_secreta)
+
 def jogo_forca():
+    if not palavra_sorteada:
+        indice_secret_word, palavra_secreta, anderlines_palavra_secreta = sortear_palavra()
     while True:
             opcoes_jogo = [
                 inquirer.List(
@@ -68,7 +78,8 @@ def jogo_forca():
             respostas = inquirer.prompt(opcoes_jogo)
 
             if respostas['escolha'] == 'Pedir dica':
-                dica()
+                dica(indice_secret_word)
+                print_forca(anderlines_palavra_secreta, 0)
 
             elif respostas['escolha'] == 'Chutar letra':
                 chute(palavra_secreta)
@@ -84,10 +95,8 @@ def dica(indice_secret_word):
     print(f'DICA =', dica)
     
 
-def chute(palavra_secreta):
-    tentativas = 6
+def chute(palavra_secreta, anderlines_palavra_secreta):
     letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'Ç', 'Sair']
-    chutes = []
     while True:
             letra = [
                 inquirer.List(
@@ -98,20 +107,15 @@ def chute(palavra_secreta):
             ]
 
             respostas = inquirer.prompt(letra)
+            letras.remove(respostas['escolha'])
+            verificar_letra(respostas['escolha'], palavra_secreta)
 
-            
-            if respostas['escolha'] not in palavra_secreta:
-                tentativas -= 1
-                print(forca_img.forca1()[1])
-                chutes.append(respostas['escolha'])
-                print(f'Chutes: {chutes}')
-                return chute()
-            else:
-                forca_img.forca1()[0]
-                chutes.append(respostas['escolha'])
-                print(f'Chutes: {chutes}')
-                return chute()
-    
+def verificar_letra(letra_escolhida, palavra_secreta):
+    if letra_escolhida in palavra_secreta:
+        for index, letra in enumerate(palavra_secreta):
+            if letra_escolhida == letra:
+                print(index, letra)
+  
 
 def add_remove():
     while True:
@@ -159,16 +163,24 @@ def remove():
     print('Qual palavra quer remover? ')
     abrir_palavra = open(r'./forca/palavras.txt', 'r', encoding='utf-8')
     abrir_dicas = open(r'./forca/dicas.txt', 'r', encoding='utf-8')
+
     print_words = abrir_palavra.readlines()
     print_tips = abrir_dicas.readlines()
+
     lista_palavras = []
     lista_dicas = []
 
     for word in print_words:
-        lista_palavras.append(word[:-1])
-    
+        if word.__contains__('\n'):
+            lista_palavras.append(word[:-1])
+        else:
+            lista_palavras.append(word)
+
     for tip in print_tips:
-        lista_dicas.append(tip[:-1])
+        if tip.__contains__('\n'):
+            lista_dicas.append(tip[:-1])
+        else:
+            lista_dicas.append(tip)
 
     palavras = [
         inquirer.List("escolha", message="Remover Palavra", choices=lista_palavras)
@@ -176,24 +188,29 @@ def remove():
 
     respostas = inquirer.prompt(palavras)['escolha']
     abrir_palavra.close()
-
+    indice_palavra = -1
+    for indice, palavra in enumerate(lista_palavras):
+        if palavra == respostas:
+            indice_palavra = indice
     lista_palavras.remove(respostas)
+
     palavras_file = open(r'./forca/palavras.txt', 'w', encoding='utf-8')
-    stringona_palavras = '\n'.join(lista_palavras)
-    palavras_file.write(stringona_palavras)
-    print(f'Palavra {stringona_palavras} removida com sucesso!')
+    string_palavras = '\n'.join(lista_palavras)
+    palavras_file.write(string_palavras)
+
+    print()
+    print(f'\033[1;32mPalavra {respostas} removida com sucesso!\033[0;0m')
     palavras_file.close()
 
 
-    # lista_dicas.remove(respostas)
-    # dicas_file = open(r'./forca/dicas.txt', 'w', encoding='utf-8')
-    # stringona_dicas = '\n'.join(lista_dicas)
-    # dicas_file.write(stringona_dicas)
-    # abrir_dicas.close()
-    
+    lista_dicas.pop(indice_palavra)
+    dicas_file = open(r'./forca/dicas.txt', 'w', encoding='utf-8')
+    string_dicas = '\n'.join(lista_dicas)
+    dicas_file.write(string_dicas)
+  
 
 def view_tips_words():
-    print('PALAVRA = DICA')
+    print('\033[1;96mPALAVRA\033[0;0m = \033[1;92mDICA\033[0;0m')
     abrir_palavra = open(r'./forca/palavras.txt', 'r', encoding='utf-8')
     abrir_dica = open(r'./forca/dicas.txt', 'r', encoding='utf-8')
     print_words = abrir_palavra.readlines()
@@ -209,7 +226,10 @@ def view_tips_words():
         lista_dicas.append(tip.split('\n'))
 
     for index in range(len(print_words)):
-        print(print_words[index].strip(),'= ' + print_tips[index].strip())
+        print(f'\033[1;96m{print_words[index].strip()}\033[0;0m = \033[1;92m{print_tips[index].strip()}\033[0;0m')
+
+    abrir_palavra.close()
+    abrir_dica.close()
 
 
 def sair():
